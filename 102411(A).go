71967bcf011a6850d014ffe55597e8e78add2524
Denis Limarev <dlimarev@wakeapp.ru>
2019-11-25 01:48:57 +0300

    fix

--- 102411(A).go
+++ 102411(A).go
@@ -16,67 +16,66 @@ func main() {
 	var err error
 	var positionA, positionB float64
 
-	for scanner.Scan() {
-		content = scanner.Text()
-		bars = strings.Split(content, " ")
-		positionA, err = strconv.ParseFloat(bars[0], 0)
-		if err != nil {
-			return
-		}
-
-		positionB, err = strconv.ParseFloat(bars[1], 0)
-		if err != nil {
-			return
-		}
-
-		length, err = strconv.ParseFloat(bars[2], 0)
-		if err != nil {
-			return
-		}
-
-		diffLen := positionB - positionA
-		count := 0
-
-		for {
-			if positionB >= length {
-				if positionA >= length {
-					break
-				}
-
-				positionA = positionB
-				count++
-				break
-			}
-
-			if positionA >= length {
-				if positionB >= length {
-					break
-				}
-
-				positionB = positionA
-				count++
-				break
-			}
-
-			if positionA > positionB {
-				positionB = positionA
-				count++
-				continue
-			}
-
-			if positionB > positionA {
-				positionA = positionB
-				count++
-				continue
-			}
-
-			if positionA == positionB {
-				positionB += diffLen
-				count++
-				continue
-			}
-		}
-
-		fmt.Println(count)
+	scanner.Scan()
+	content = scanner.Text()
+	bars = strings.Split(content, " ")
+	positionA, err = strconv.ParseFloat(bars[0], 0)
+	if err != nil {
+		return
 	}
+
+	positionB, err = strconv.ParseFloat(bars[1], 0)
+	if err != nil {
+		return
+	}
+
+	length, err = strconv.ParseFloat(bars[2], 0)
+	if err != nil {
+		return
+	}
+
+	diffLen := positionB - positionA
+	count := 0
+
+	for {
+		if positionB >= length {
+			if positionA >= length {
+				break
+			}
+
+			positionA = positionB
+			count++
+			break
+		}
+
+		if positionA >= length {
+			if positionB >= length {
+				break
+			}
+
+			positionB = positionA
+			count++
+			break
+		}
+
+		if positionA > positionB {
+			positionB = positionA
+			count++
+			continue
+		}
+
+		if positionB > positionA {
+			positionA = positionB
+			count++
+			continue
+		}
+
+		if positionA == positionB {
+			positionB += diffLen
+			count++
+			continue
+		}
+	}
+
+	fmt.Println(count)
 }
