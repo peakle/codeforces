550010c76625f4600bd51455f1208a37e8eabc9c
unknown <denislimarev@gmail.com>
2019-12-26 01:17:46 +0300

    edit 102461(B)

--- 102461(B).go
+++ 102461(B).go
@@ -10,15 +10,12 @@ import (
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	buf := make([]byte, 1000000000)
-	scanner.Buffer(buf, len(buf))
-
 	scanner.Scan()
 	n, _ := strconv.Atoi(scanner.Text())
 
 	var data []string
-	var l1, r1, l2, r2, s1, d1, s2, d2, lm, rm, ld int
-	for i := 0; i < n; i++ {
+	var l1, r1, l2, r2, d1, d2 int
+	for testNum := 0; testNum < n; testNum++ {
 		scanner.Scan()
 		data = strings.Split(scanner.Text(), " ")
 		l1, _ = strconv.Atoi(data[0])
@@ -28,36 +25,52 @@ func main() {
 
 		scanner.Scan()
 		data = strings.Split(scanner.Text(), " ")
-		s1, _ = strconv.Atoi(data[0])
+		s1, _ := strconv.Atoi(data[0])
 		d1, _ = strconv.Atoi(data[1])
-		s2, _ = strconv.Atoi(data[2])
+		s2, _ := strconv.Atoi(data[2])
 		d2, _ = strconv.Atoi(data[3])
 
-		lm = getMin(l1, l2)
-		rm = getMax(r1, r2)
-		ld = lm - rm
-
-		if ld < (d1 + d2) {
-			fmt.Println(-1, -1)
+		if s1+d1 <= s2 {
+			fmt.Println(s1, s2)
 			continue
 		}
 
-		fmt.Println()
+		if s1 >= s2+d2 {
+			fmt.Println(s1, s2)
+			continue
+		}
+
+		rList := make([]int, 2)
+		rList[0], rList[1] = -1, -1
+
+		var sDiv, minDiv int
+		minDiv = abs(r1-l1) + abs(r2-l2)
+
+		for i := l1; i+d1 <= r1; i++ {
+			for j := l2; j+d2 <= r2; j++ {
+				if i+d1 <= j || i >= j+d2 {
+					sDiv = abs(s1-i) + abs(s2-j)
+
+					if minDiv > sDiv {
+						rList[0], rList[1] = i, j
+						minDiv = sDiv
+					}
+				}
+			}
+
+			if minDiv == 0 {
+				break
+			}
+		}
+
+		fmt.Println(rList[0], rList[1])
 	}
 }
 
-func getMax(f, t int) int {
-	if f > t {
-		return f
+func abs(n int) int {
+	if n < 0 {
+		return n * -1
 	}
 
-	return t
-}
-
-func getMin(f, t int) int {
-	if f > t {
-		return t
-	}
-
-	return f
+	return n
 }
