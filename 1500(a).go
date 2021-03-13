cbf06c40ca10c742d77285eb1f5395117f98f368
Denis <dlimarev@wakeapp.ru>
2021-03-14 00:20:29 +0300

    add 1500a

---
+++ 1500(a).go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type setType struct {
+	s1, s2 int
+}
+
+var ar []int
+
+const maxTokenSize int = 2500000 * 7
+
+func main() {
+	var size int
+	fmt.Scan(&size)
+
+	var ar = make([]int, 0, size)
+	var sums = make(map[int]setType, size)
+	var val int
+
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Buffer(make([]byte, 0, maxTokenSize), maxTokenSize)
+	scanner.Scan()
+	for _, v := range strings.Split(scanner.Text(), " ") {
+		val, _ = strconv.Atoi(v)
+		ar = append(ar, val)
+	}
+
+	for s1, v1 := range ar {
+		for e1, v2 := range ar {
+			if s1 >= e1 {
+				continue
+			}
+
+			sum := v1 + v2
+			if set, ok := sums[sum]; ok && s1 != set.s1 && s1 != set.s2 && e1 != set.s1 && e1 != set.s2 {
+				fmt.Println("YES")
+				fmt.Println(s1+1, e1+1, set.s1+1, set.s2+1)
+				return
+			} else if ok == false {
+				sums[sum] = setType{s1: s1, s2: e1}
+			}
+		}
+	}
+
+	fmt.Println("NO")
+}
