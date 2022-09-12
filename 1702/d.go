34a2c843f8f90537d7c5b0eedcfb9a7f7bf08233
Denis <d.limarev@corp.mail.ru>
2022-09-12 14:24:07 +0300

    add solution for 1702(d)

--- 1702/d.go
+++ 1702/d.go
@@ -4,8 +4,14 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 )
 
+const firstLetter = 'a' - 1
+
+//1
+//zzcd
+//52
 func main() {
 	var in = bufio.NewReader(os.Stdin)
 
@@ -14,11 +20,53 @@ func main() {
 
 	for ; testNum > 0; testNum-- {
 		var s string
-		fmt.Fscan(in, &s )
+		fmt.Fscan(in, &s)
 
-		var p int64
+		var p int
 		fmt.Fscan(in, &p)
 
+		var w int
+		for _, l := range s {
+			v := int(l - firstLetter)
+			w += v
+		}
 
+		d := p - w
+		if d >= 0 {
+			fmt.Println(s)
+			continue
+		}
+
+		ss := []rune(s)
+		sort.Slice(ss, func(i, j int) bool {
+			return ss[i] > ss[j]
+		})
+
+		del := make(map[rune]int, 10)
+		for _, sss := range ss {
+			if w > p {
+				w -= int(sss - firstLetter)
+				del[sss]++
+			} else {
+				break
+			}
+		}
+
+		se := []rune(s)
+		for i, v := range se {
+			if vv, ok := del[v]; ok && vv > 0 {
+				del[v]--
+				se[i] = 0
+			}
+		}
+
+		res := make([]rune, 0, len(se))
+		for _, ss := range se {
+			if ss != 0 {
+				res = append(res, ss)
+			}
+		}
+
+		fmt.Println(string(res))
 	}
 }
